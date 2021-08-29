6a03cb70e49d66141bc2a0b5148216983255fac1
Ipsky <yap@usc.edu>
2021-08-29 15:58:29 -0700

    copy in test block

--- test.ts
+++ test.ts
@@ -7,9 +7,88 @@ import {
 } from './index.js';
 import test from 'tape';
 
-tape('example', (t) => {
-    t.deepEqual({}, {
-        fromMarkdown,
-    });
-    t.end();
+test('example', (t) => {
+    t.deepEqual(
+        removePosition(
+            fromMarkdown('a ~~b~~ c.', {
+                mdastExtensions: [pandocMarkFromMarkdown],
+            }),
+            true
+        ),
+        {
+            type: 'root',
+            children: [
+                {
+                    type: 'paragraph',
+                    children: [
+                        { type: 'text', value: 'a ' },
+                        { type: 'delete', children: [{ type: 'text', value: 'b' }] },
+                        { type: 'text', value: ' c.' }
+                    ]
+                }
+            ]
+        },
+        'should support strikethrough'
+    )
+
+    t.deepEqual(
+        removePosition(
+            fromMarkdown('a ~~b\nc~~ d.', {
+                mdastExtensions: [pandocMarkFromMarkdown],
+            }),
+            true
+        ),
+        {
+            type: 'root',
+            children: [
+                {
+                    type: 'paragraph',
+                    children: [
+                        { type: 'text', value: 'a ' },
+                        { type: 'delete', children: [{ type: 'text', value: 'b\nc' }] },
+                        { type: 'text', value: ' d.' }
+                    ]
+                }
+            ]
+        },
+        'should support strikethrough w/ eols'
+    )
+
+    t.end()
+})
+
+test('mdast -> markdown', (t) => {
+    t.deepEqual(
+        toMarkdown(
+            {
+                type: 'paragraph',
+                children: [
+                    { type: 'text', value: 'a ' },
+                    { type: 'delete', children: [{ type: 'text', value: 'b' }] },
+                    { type: 'text', value: ' c.' }
+                ]
+            },
+            { extensions: [pandocMarkToMarkdown] }
+        ),
+        'a ~~b~~ c.\n',
+        'should serialize strikethrough'
+    )
+
+    t.deepEqual(
+        toMarkdown(
+            {
+                type: 'paragraph',
+                children: [
+                    { type: 'text', value: 'a ' },
+                    { type: 'delete', children: [{ type: 'text', value: 'b\nc' }] },
+                    { type: 'text', value: ' d.' }
+                ]
+            },
+            { extensions: [pandocMarkToMarkdown] }
+        ),
+        'a ~~b\nc~~ d.\n',
+        'should serialize strikethrough w/ eols'
+    )
+
+    t.end()
 });
