53a2ff39bf30a3acf76ecc2e19f868d2c0e66cd8
Ipsky <yap@usc.edu>
2021-08-29 13:33:11 -0700

    declare extension

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { PhrasingContent, StaticPhrasingContentMap, Parent } from "mdast";
+
+declare module 'mdast' {
+    interface StaticPhrasingContentMap {
+        mark: Mark;
+    }
+}
+
+export interface Mark extends Parent {
+    type: 'mark';
+    children: PhrasingContent[];
+}
