0fa6d1a3d90f97801af848228342dfafcec057b4
Ipsky <yap@usc.edu>
2021-08-29 15:58:23 -0700

    fix imports

--- test.ts
+++ test.ts
@@ -1,7 +1,11 @@
-import tape = require('tape');
 import { fromMarkdown } from 'mdast-util-from-markdown';
 import { toMarkdown } from 'mdast-util-to-markdown';
 import { removePosition } from 'unist-util-remove-position';
+import {
+    pandocMarkFromMarkdown,
+    pandocMarkToMarkdown,
+} from './index.js';
+import test from 'tape';
 
 tape('example', (t) => {
     t.deepEqual({}, {
