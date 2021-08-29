9c3497ec20edf6175db984b41b45d6509f0fe645
Ipsky <yap@usc.edu>
2021-08-29 14:20:36 -0700

    add import

--- test.ts
+++ test.ts
@@ -1,6 +1,7 @@
 import tape = require('tape');
 import { fromMarkdown } from 'mdast-util-from-markdown';
 import { toMarkdown } from 'mdast-util-to-markdown';
+import { removePosition } from 'unist-util-remove-position';
 
 tape('example', (t) => {
     t.deepEqual(1, 1);
