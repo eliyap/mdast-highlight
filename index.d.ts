21a07d3ad2ded812e6286cde768195600ed9c884
Ipsky <yap@usc.edu>
2021-08-29 13:33:53 -0700

    add declaration

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,9 @@
 import { PhrasingContent, Parent } from "mdast";
-
 declare module 'mdast' {
     interface StaticPhrasingContentMap {
         mark: Mark;
     }
 }
-
 export interface Mark extends Parent {
     type: 'mark';
     children: PhrasingContent[];
